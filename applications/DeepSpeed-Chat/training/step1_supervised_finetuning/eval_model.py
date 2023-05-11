1010fb267b774e02b15db9a8c2b4b92da6cf6d68
dmitrymailk <dmitrymail_k@mail.ru>
2023-05-11 05:24:31 +0300

    add examples

--- applications/DeepSpeed-Chat/training/step1_supervised_finetuning/eval_model.py
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/eval_model.py
@@ -4,28 +4,20 @@ from datasets import load_dataset, load_from_disk
 import torch
 
 
-def generate(
-    model,
-    tokenizer,
-    inputs,
-    num_beams=1,
-    num_beam_groups=1,
-    do_sample=False,
-    num_return_sequences=1,
-    max_new_tokens=100,
-):
+def generate(model, tokenizer, inputs, device="cuda"):
     with torch.no_grad():
         generate_ids = model.generate(
-            inputs.input_ids.cuda(),
-            num_beams=num_beams,
-            num_beam_groups=num_beam_groups,
-            do_sample=do_sample,
-            num_return_sequences=num_return_sequences,
-            max_new_tokens=max_new_tokens,
+            inputs.input_ids.to(device),
+            penalty_alpha=0.25,
+            top_k=4,
+            repetition_penalty=1.1,
+            max_new_tokens=512,
         )
 
         result = tokenizer.batch_decode(
-            generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False
+            generate_ids,
+            skip_special_tokens=True,
+            clean_up_tokenization_spaces=False,
         )
         return result
 
@@ -41,12 +33,15 @@ def extract_answer(g_answer: str):
 
 
 def compare_baseline_finetuned():
-    ru_instruct = load_from_disk(
-        "/home/kosenko/deepspeed/DeepSpeedExamples/applications/DeepSpeed-Chat/training/step1_supervised_finetuning/datasets/databricks_dolly_15k_translated_fixed"
-    )
+    # ru_instruct = load_from_disk(
+    #     "/home/kosenko/deepspeed/DeepSpeedExamples/applications/DeepSpeed-Chat/training/step1_supervised_finetuning/datasets/databricks_dolly_15k_translated_fixed"
+    # )
+    ru_instruct = load_dataset("IlyaGusev/ru_turbo_alpaca")
+    ru_instruct = ru_instruct["train"].filter(lambda x: x["label"] == "ok")
+    ru_instruct = ru_instruct.train_test_split(test_size=500, seed=42)
     ru_instruct = ru_instruct["test"]
-    baseline_model = "facebook/xglm-1.7B"
-    finetuned_model = "/home/kosenko/deepspeed/DeepSpeedExamples/applications/DeepSpeed-Chat/training/step1_supervised_finetuning/models/xglm-1.7B_ru_v2"
+    baseline_model = "facebook/xglm-4.5B"
+    finetuned_model = "/home/kosenko/deepspeed/DeepSpeedExamples/applications/DeepSpeed-Chat/training/step1_supervised_finetuning/models/xglm-4.5B_ru_v5/"
 
     table = {
         "baseline_model": [],
@@ -57,8 +52,9 @@ def compare_baseline_finetuned():
         "finetuned_model_output": [],
     }
 
-    def get_model(model_path, tokenizer):
-        model = AutoModelForCausalLM.from_pretrained(model_path, device_map="auto")
+    def get_model(model_path, tokenizer, device):
+        model = AutoModelForCausalLM.from_pretrained(model_path)
+        model.to(device)
 
         tokenizer.pad_token = tokenizer.eos_token
         model.config.end_token_id = tokenizer.eos_token_id
@@ -68,32 +64,33 @@ def compare_baseline_finetuned():
 
     tokenizer = AutoTokenizer.from_pretrained(baseline_model, fast_tokenizer=True)
 
-    model_baseline = get_model(baseline_model, tokenizer)
-    model_finetuned = get_model(finetuned_model, tokenizer)
+    model_baseline = get_model(baseline_model, tokenizer, "cuda:0")
+    model_finetuned = get_model(finetuned_model, tokenizer, "cuda:1")
     model_baseline.eval()
     model_finetuned.eval()
-    total_steps = 250
+    total_steps = len(ru_instruct)
     print_results = True
 
     for i, example in enumerate(ru_instruct):
         print(f"{i+1}/{total_steps} - {(1+i)/total_steps*100}%")
 
-        prompt = f"Human: {example['context_translated']} {example['instruction_translated']} Assistant: "
+        prompt = f"Human: {example['instruction']} {example['input']} Assistant:"
 
-        answer = example["response_translated"]
-        inputs = tokenizer(prompt, return_tensors="pt")
+        answer = example["output"]
+        inputs = tokenizer(
+            prompt,
+            return_tensors="pt",
+            truncation=True,
+            max_length=512,
+        )
         baseline_result = generate(
-            model_baseline,
-            tokenizer=tokenizer,
-            inputs=inputs,
+            model_baseline, tokenizer=tokenizer, inputs=inputs, device="cuda:0"
         )
         baseline_result = baseline_result[0]
         baseline_result = extract_answer(baseline_result)
 
         finetuned_result = generate(
-            model_finetuned,
-            tokenizer=tokenizer,
-            inputs=inputs,
+            model_finetuned, tokenizer=tokenizer, inputs=inputs, device="cuda:1"
         )
         finetuned_result = finetuned_result[0]
         finetuned_result = extract_answer(finetuned_result)
@@ -107,9 +104,9 @@ def compare_baseline_finetuned():
         if print_results:
             print()
             print(prompt)
-            print(baseline_result)
-            print(finetuned_result)
-            print(answer)
+            print("Baseline: ", baseline_result)
+            print("Finetuned: ", finetuned_result)
+            print("Real: ", answer)
             print("=" * 100)
             print("=" * 100)
         if i > total_steps:
@@ -121,3 +118,4 @@ def compare_baseline_finetuned():
 
 if __name__ == "__main__":
     print("Starting evaluation.")
+    compare_baseline_finetuned()
--- applications/DeepSpeed-Chat/training/step1_supervised_finetuning/evaluation_scripts/eval_model.sh
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/evaluation_scripts/eval_model.sh
@@ -5,7 +5,7 @@
 # DeepSpeed Team
 
 # You can provide two models to compare the performance of the baseline and the finetuned model
-# export CUDA_VISIBLE_DEVICES=0
-model_name="xglm-1.7B_ru_v2"
+# export CUDA_VISIBLE_DEVICES=1
+model_name="xglm-4.5B_ru_v5"
 log_path="./models/$model_name/eval.log"
 nohup python -u eval_model.py > $log_path &
