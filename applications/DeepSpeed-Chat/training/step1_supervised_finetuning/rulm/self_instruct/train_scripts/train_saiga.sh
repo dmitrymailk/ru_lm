42e30174e8dc3ab3bbcf5ce4e09346b1c1f9eb53
dmitrymailk <dmitrymail_k@mail.ru>
2023-07-09 22:27:05 +0300

    run int8 training

---
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/rulm/self_instruct/train_scripts/train_saiga.sh
@@ -0,0 +1,9 @@
+export CUDA_VISIBLE_DEVICES=2,3
+export WANDB_BASE_URL="https://api.wandb.ai"
+export WANDB_PROJECT="saiga_self_instruct"
+
+python -m src.train --config-file configs/saiga_7b.json \
+	--train-file ./train.jsonl \
+	--val-file valid.jsonl  \
+	--output-dir models/saiga_7b_v1 \
+	--omit-base-model-save
