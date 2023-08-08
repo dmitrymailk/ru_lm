9b4c5bce1e8ce539a4c157811fda12b5876c445a
dmitrymailk <dmitrymail_k@mail.ru>
2023-08-08 18:52:51 +0300

    add example

--- applications/DeepSpeed-Chat/training/step1_supervised_finetuning/instruct_models.py
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/instruct_models.py
@@ -98,17 +98,6 @@ def generate(model, tokenizer, prompt, generation_config):
     output = tokenizer.decode(output_ids, skip_special_tokens=True)
     return output.strip()
 
-def generate(model, tokenizer, prompt, generation_config):
-    data = tokenizer(prompt, return_tensors="pt")
-    data = {k: v.to(model.device) for k, v in data.items()}
-    output_ids = model.generate(
-        **data,
-        generation_config=generation_config
-    )[0]
-    output_ids = output_ids[len(data["input_ids"][0]):]
-    output = tokenizer.decode(output_ids, skip_special_tokens=True)
-    return output.strip()
-
 
 def add_special_tokens_v2(string):
     string = string.replace("\n", "</s>")
